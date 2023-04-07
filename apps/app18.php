a2fe6ede4a5f5c6731dee0b3e15bb23f9a491dd1
Your Name <you@example.com>
2023-04-06 19:28:24 -0600

    dash

---
+++ apps/app18.php
@@ -0,0 +1,70 @@
+<?php
+session_set_cookie_params(0, '/', '.4ks.online');
+session_start();
+
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    // Store the current page in a session variable
+    $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
+    header('Location: /');
+    exit;
+}
+
+// User is logged in, display the app content here
+$username = $_SESSION['username'];
+
+// Clear the redirect session variable if it exists
+if (isset($_SESSION['redirect'])) {
+    unset($_SESSION['redirect']);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>App1</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+		}
+		
+		iframe {
+			border: none;
+			width: 100vw;
+			height: calc(100vh - 60px);
+			margin-top: 60px;
+		}
+
+		.navbar {
+			background-color: #333;
+			position: fixed;
+			top: 0;
+			left: 0;
+			right: 0;
+			height: 60px;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 20px;
+			color: #fff;
+		}
+
+		.navbar a {
+			color: #fff;
+			text-decoration: none;
+			margin-right: 20px;
+		}
+
+	</style>
+</head>
+<body>
+	<nav class="navbar">
+		<a href="../dashboard.php">Dashboard</a>
+		<a href="../logout.php">Logout</a>
+	</nav>
+
+	<iframe src="https://4ks.mx" frameborder="0"></iframe>
+</body>
+</html>
